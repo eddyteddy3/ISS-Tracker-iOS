34cd639cd3b430969cb539c8a489798379b378ea
Mouzzam Tahir <moazzamtahir81@gmail.com>
2020-05-14 04:37:42 +0500

    added Button

--- ISS Tracker/Controller/ViewController.swift
+++ ISS Tracker/Controller/ViewController.swift
@@ -9,13 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK: UI Elements are declared here.
+    var tapButton: UIButton?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
+        
+        initializeUI()
     }
-
+    
+    func initializeUI() {
+        
+        tapButton = UIButton(type: .system)
+        tapButton?.setTitle("Track ISS", for: .normal)
+        tapButton?.backgroundColor = .magenta
+        tapButton?.layer.cornerRadius = 5
+        tapButton?.translatesAutoresizingMaskIntoConstraints = false
+        tapButton?.addTarget(self, action: #selector(doSomeThing), for: .touchUpInside)
+        
+        view.addSubview(tapButton!)
+        
+        NSLayoutConstraint.activate([
+            tapButton!.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor),
+            tapButton!.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            tapButton!.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
+        ])
+    }
+    
+    @objc
+    func doSomeThing() {
+    }
 
 }
 
