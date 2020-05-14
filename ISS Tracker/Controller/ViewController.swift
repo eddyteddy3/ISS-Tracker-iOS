34db4e6bc2bfca05f2c8a535eb611b64ae21a8b0
Mouzzam Tahir <moazzamtahir81@gmail.com>
2020-05-15 01:27:35 +0500

    added ISS Header Label

--- ISS Tracker/Controller/ViewController.swift
+++ ISS Tracker/Controller/ViewController.swift
@@ -11,7 +11,27 @@ import UIKit
 class ViewController: UIViewController {
     
     //MARK: UI Elements are declared here.
-    var tapButton: UIButton?
+    var trackISSButton: UIButton = {
+        let trackButton = UIButton(type: .system)
+        trackButton.setTitle("Track ISS", for: .normal)
+        trackButton.backgroundColor = .magenta
+        trackButton.layer.cornerRadius = 5
+        trackButton.translatesAutoresizingMaskIntoConstraints = false
+        trackButton.addTarget(self, action: #selector(doSomeThing), for: .touchUpInside)
+        
+        return trackButton
+    }()
+    
+    var ISSHeaderLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.text = "ISS Coordinates"
+        label.font = UIFont.boldSystemFont(ofSize: 40)
+        label.adjustsFontSizeToFitWidth = true
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,20 +42,16 @@ class ViewController: UIViewController {
     }
     
     func initializeUI() {
-        
-        tapButton = UIButton(type: .system)
-        tapButton?.setTitle("Track ISS", for: .normal)
-        tapButton?.backgroundColor = .magenta
-        tapButton?.layer.cornerRadius = 5
-        tapButton?.translatesAutoresizingMaskIntoConstraints = false
-        tapButton?.addTarget(self, action: #selector(doSomeThing), for: .touchUpInside)
-        
-        view.addSubview(tapButton!)
+        view.addSubview(trackISSButton)
+        view.addSubview(ISSHeaderLabel)
         
         NSLayoutConstraint.activate([
-            tapButton!.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor),
-            tapButton!.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
-            tapButton!.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
+            trackISSButton.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor),
+            trackISSButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            trackISSButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
+            
+            ISSHeaderLabel.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            ISSHeaderLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 45)
         ])
     }
     
